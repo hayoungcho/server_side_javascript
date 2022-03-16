1ab19edaa8851222b093a3875fe9a171c041317c
jeong06090 <jeong06090@naver.com>
2022-03-16 23:55:52 +0900

    save

--- calleback.js
+++ calleback.js
@@ -1,3 +1,20 @@
 //callback
 
 a = [3, 2, 1];
+
+//오름차순 정렬
+a.sort(function(v1, v2){
+  return v1 - v2;
+});
+console.log(a);
+
+//내림차순 정렬
+a.sort(function(v1, v2){
+  return v2 - v1;
+});
+console.log(a);
+
+function b(v1, v2){
+  return v1 -v2;
+}
+a.sort(b);
