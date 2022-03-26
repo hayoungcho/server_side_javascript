f2f986507dbb3eefcd5b52c269bae3e3aec5d9a5
jeong06090 <jeong06090@naver.com>
2022-03-26 23:28:38 +0900

    20220326 express template engine npm install pug --save

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 //root file, entry file..
 var express = require('express');
 var app = express();
-
+app.locals.pretty= true;
+//Express 템플리트 렌더링(Jade > pug)
+app.set('view engine', 'pug');
+//jade파일의 디렉토리가 됨
+app.set('views', './views');
 //정적인 파일이 위치한 directory를 지정하는 기능
 //static('directory 이름')
 app.use(express.static('public'));
+
+//temp파일을 랜더링하여 웹 페이지로 전송
+app.get('/template', function(req, res){
+  //res('file', {property & value})
+  res.render('temp', {time : Date(), _title :'jade'});
+});
 
 //url,  after call function(req, res)
 app.get('/', function(req, res){
